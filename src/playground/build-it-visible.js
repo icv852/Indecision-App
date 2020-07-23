bc2d4943781ee9724fbdbfdb61b850483cb22964
icv852 <icv852@gmail.com>
2020-07-23 11:27:08 +0800

    First Commit

---
+++ src/playground/build-it-visible.js
@@ -0,0 +1,22 @@
+let visibility = false
+
+const toggleVisibility = () => {
+    visibility = !visibility
+    render()
+}
+
+const render = () => {
+    const jsx = (
+        <div>
+            <h1>Visibility Toggle</h1>
+        <button onClick={toggleVisibility}>{visibility ? 'Hide message' : 'Show message'}</button>
+            {visibility &&
+                <p>Here is the secret message!</p>
+            }
+        </div>
+    )
+    
+    ReactDOM.render(jsx, document.getElementById('app'))
+}
+
+render()
